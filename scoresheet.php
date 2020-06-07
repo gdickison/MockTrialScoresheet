5c9409dd443d9eb548d77d9ad96b6130e21673b3
Gregory Dickison <gregorydickison@Gregorys-Air.frontierlocal.net>
2020-06-06 19:22:38 -0700

    Change some class names, add php to echo the input, and add comment sections.

--- scoresheet.php
+++ scoresheet.php
@@ -1,9 +1,13 @@
 <?php
     if(isset($_POST['submit'])){
         echo "<h1>Data</h1>";
-        $scoreSelectRound = $_POST['score_select_round'];
+        echo "<h2>Round No: " . $_POST['round_number'] . "</h2>";
+        echo "<h2>Courtroom No: " . $_POST['courtroom_number'] . "</h2>";
+        echo "<h2>Plaintiff: " . $_POST['plaintiff_code'] . "</h2>";
+        echo "<h2>Defendant: " . $_POST['defendant_code'] . "</h2>";
+
         foreach($_POST as $name => $value){
-            echo $name . " " . $value . "</br>";
+            echo ucwords(str_replace('_', ' ', $name)) . " " . $value . "</br>";
         }
     }
 ?>
