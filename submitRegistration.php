f5e834c7d22888cc62e2088a446575ef60ce585b
Gregory Dickison <gregorydickison@Gregorys-Air.frontierlocal.net>
2020-06-23 20:30:42 -0700

    The qCheckForSchool is not working and I don't know why

--- submitRegistration.php
+++ submitRegistration.php
@@ -3,25 +3,37 @@
 
     $sSchoolName = $_POST['school_name'];
 
-    $qNewSchool = "INSERT INTO school (
-        school_name
-    ) VALUES (
-        '$sSchoolName'
-    );";
+    $qCheckForSchool = "SELECT * FROM school WHERE school_name LIKE CONCAT('%', '$_POST[school_name]', '%');";
 
-    mysqli_query($con, $qNewSchool);
+    $res = mysqli_query($con, $qCheckForSchool);
 
-    $x = "A";
-    for($i = 0; $i < $_POST[num_teams]; $i++){
-        $sTeamCode = teamCodeGenerator();
-        $newTeam = "INSERT INTO team_codes (
-            team_name,
-            team_code
-        ) VALUES (
-            CONCAT('$sSchoolName', ' ' , '$x'),
-            '$sTeamCode'
-        );";
-        mysqli_query($con, $newTeam);
-        $x++;
+    if(mysqli_num_rows($res) > 0){
+        echo "That school is already registered";
     }
+    else {
+        $qNewSchool = "INSERT INTO school (
+            school_name
+        ) VALUES (
+            '$sSchoolName'
+        );";
+
+        mysqli_query($con, $qNewSchool);
+
+        $x = "A";
+        for($i = 0; $i < $_POST[num_teams]; $i++){
+            $sTeamCode = teamCodeGenerator();
+            $newTeam = "INSERT INTO team_codes (
+                team_name,
+                team_code
+            ) VALUES (
+                CONCAT('$sSchoolName', ' ' , '$x'),
+                '$sTeamCode'
+            );";
+            mysqli_query($con, $newTeam);
+            $x++;
+        }
+
+        echo "Congratulations. You have registered " . $_POST[num_teams] . " teams for " . $sSchoolName . ".";
+    }
+
 ?>
